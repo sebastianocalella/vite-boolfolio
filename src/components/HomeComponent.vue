1980eedcd2c7a07ed44eb0d557f2f914eeea1cbf
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-02 16:42:18 +0100

    insert projects content in homeComponent

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,8 +2,15 @@
     <div class="container">
         <h2>Projects:</h2>
         <div class="row justify-content-around">
-            <div class="card col-5">
-
+            <div v-for="project in projects" class="card col-5 mb-5">
+                <img class="card-img-top" :src="project.image_path" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.description }}</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary">Show more</a>
+                </div>
             </div>
         </div>
 
@@ -22,27 +29,25 @@ export default {
         }
     },
 
-    methods:{
-        getProjects(){
+    methods: {
+        getProjects() {
             axios.get(this.apiAddress, {
-                params:{
+                params: {
 
                 }
             })
-            .then(function(response){
-                console.log(response)
-            })
-            .catch(function(error){
-                console.warn(error)
-            })
+                .then((response) => {
+                    this.projects = response.data.results.data
+                })
+                .catch(function (error) {
+                    console.warn(error)
+                })
         }
     },
 
-    created(){
+    created() {
         this.getProjects()
     }
 }
 </script>
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
