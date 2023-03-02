0f65c376d440d4929d65ffe071270ed0ba511e0b
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-02 16:10:02 +0100

    start implementing vue elements

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+    import HomeComponent from './components/HomeComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeComponent,
+  },
+  data() {
+    return {
+      
+    }
+  },
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>cioa</h1>
+  <HomeComponent/>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './styles/general.scss' as *;
 </style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Projects</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'HomeComponent',
+
+    data() {
+        return {
+            projects: []
+        }
+    },
+
+    methods: {
+
+    },
+
+    created: {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
