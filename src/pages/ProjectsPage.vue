9397069c7b9da96b958cb75fd705d5d701809d35
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-03 15:40:42 +0100

    preparing projectsPage axios call

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -6,8 +6,38 @@
     </div>
 </template>
 <script>
+
+import ProjectComponent from '../components/ProjectComponent.vue';
+import axios from 'axios';
+
 export default {
-    name: 'Projectspage'
+    name: 'Projectspage',
+    components:{
+        ProjectComponent
+    },
+    data(){
+        return{
+            projects: [],
+            urlAddress: 'http://127.0.0.1:8000'
+        }
+    },
+    methods: {
+        getProjects(){
+            axios.get(this.urlAddress + '/api/projects/', {
+                params: {
+                }
+            })
+            .then((response) => {
+                this.projects = response.data.results.data;
+            })
+            .catch(function (error) {
+                console.warn(error);
+            });
+        }
+    },
+    created() {
+        this.getProjects();
+    },
 }
 </script>
 <style lang="scss">
